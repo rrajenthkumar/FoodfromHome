d69cba1e4874754630af27594ff7368867403641
rajenth <rkumar@aidminutes.com>
2023-11-17 16:27:16 +0100

    authentication wip

---
+++ backend/food_from_home/lib/food_from_home/auth/guardian.ex
@@ -0,0 +1,18 @@
+defmodule FoodFromHome.Auth.Guardian do
+  @moduledoc """
+  This is the main Guardian module used by the application to gain access to claims,
+  identity, token, etc.
+  Implements callback to properly integrate with Auth0.
+  """
+  use Guardian, otp_app: :orders
+
+  alias FoodFromHome.Auth.Identity
+
+  @impl Guardian
+  @doc false
+  def subject_for_token(user, _claims), do: {:ok, to_string(user.id)}
+
+  @impl Guardian
+  @doc false
+  def resource_from_claims(%{"sub" => "auth0|" <> user_id} = _claims), do: {:ok, %Identity{user_id: user_id}}
+end
