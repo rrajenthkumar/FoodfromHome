904884f368a0360cc75d89cdbdc670bd3c069eec
rajenth <rkumar@aidminutes.com>
2023-11-24 12:55:16 +0100

    wip

---
+++ backend/food_from_home/lib/food_from_home/kafka_agent/consumers.ex
@@ -0,0 +1,12 @@
+defmodule FoodFromHome.KafkaAgent.Consumers do
+  @moduledoc """
+  All Kafka consumer functions
+  """
+  def handle_messages(messages) do
+    for %{key: key, value: value} = message <- messages do
+      IO.inspect message
+      IO.puts "#{key}: #{value}"
+    end
+    :ok # Important!
+  end
+end
