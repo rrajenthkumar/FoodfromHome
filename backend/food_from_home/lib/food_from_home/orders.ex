dce09694383ac5ff439ff2dc616be3db3d1d0c16
rajenth <rkumar@aidminutes.com>
2023-12-02 13:18:48 +0100

    comments added

--- backend/food_from_home/lib/food_from_home/orders.ex
+++ backend/food_from_home/lib/food_from_home/orders.ex
@@ -14,12 +14,12 @@ defmodule FoodFromHome.Orders do
   defdelegate list(user, filters), to: OrderRepo
   defdelegate get!(order_id), to: OrderRepo
   defdelegate update(order, attrs), to: OrderRepo
-  # Used when last cart item is deleted in an open order
+  # Used by CartItems.Services.DeleteLastCartItemAndDeleteRelatedOpenOrder
   defdelegate delete(order), to: OrderRepo
 
   def update_delivery_address(order, delivery_address), do: UpdateDeliveryAddress.call(order, delivery_address)
   def mark_as_ready_for_pickup(order), do: SetReadyForPickupStatus.call(order)
-  # Used after payment is successful
+  # Used by payment module after payment is successful
   def confirm(order, invoice_link), do: SetConfirmedStatusAndAddInvoiceLinkAndProduceOrderConfirmedEvent.call(order, invoice_link)
   def cancel(order, seller_remark), do: SetCancelledStatusAndAddSellerRemarkAndProduceOrderCancelledEvent.call(order, seller_remark)
   def reserve_for_pickup(order, deliverer_user), do: SetReservedForPickupStatusAndCreateDelivery.call(order, deliverer_user)
