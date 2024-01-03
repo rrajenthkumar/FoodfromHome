29ca0cd58e2c9a5a559e49bf2d958b3d360532c2
rajenth <rkumar@aidminutes.com>
2024-01-03 09:30:12 +0100

    order context wip

--- backend/food_from_home/lib/food_from_home_web/controllers/order_json.ex
+++ backend/food_from_home/lib/food_from_home_web/controllers/order_json.ex
@@ -1,23 +1,23 @@
 defmodule FoodFromHomeWeb.OrderJSON do
-  alias FoodFromHome.Deliveries.Delivery
   alias FoodFromHome.CartItems.CartItem
+  alias FoodFromHome.Deliveries.Delivery
   alias FoodFromHome.FoodMenus.FoodMenu
+  alias FoodFromHome.FoodMenus.FoodMenu.Rebate
   alias FoodFromHome.Orders.Order
   alias FoodFromHome.Orders.Order.DeliveryAddress
   alias FoodFromHome.Reviews.Review
   alias FoodFromHome.Sellers.Seller
   alias FoodFromHome.Users.User
-  alias FoodFromHome.Users.User.Address
 
   @doc """
-  Renders a list of orders with limited details.
+  Renders a list of orders with limited data
   """
   def index(%{orders: orders}) do
     %{data: for(order <- orders, do: limited_data(order))}
   end
 
   @doc """
-  Renders a single order.
+  Renders a single order
   """
   def show(%{order: order}) do
     %{data: data(order)}
@@ -26,12 +26,11 @@ defmodule FoodFromHomeWeb.OrderJSON do
   defp limited_data(order = %Order{}) do
     %{
       id: order.id,
-      date: order.inserted_at,
+      created_at: order.inserted_at,
       status: order.status
     }
   end
 
-  # To render order details with preloads
   defp data(
          order = %Order{
            seller: {%Seller{} = seller, seller_user: %User{} = seller_user},
@@ -40,10 +39,11 @@ defmodule FoodFromHomeWeb.OrderJSON do
            delivery: {%Delivery{} = delivery, deliverer_user: %User{} = deliverer_user},
            review: %Review{} = review
          }
-       ) do
+       ) when is_list(cart_items) do
     %{
       id: order.id,
-      date: order.inserted_at,
+      created_at: order.inserted_at,
+      updated_at: order.updated_at,
       status: order.status,
       seller: data(seller),
       seller_user: data(seller_user),
@@ -61,7 +61,8 @@ defmodule FoodFromHomeWeb.OrderJSON do
   defp data(order = %Order{}) do
     %{
       id: order.id,
-      date: order.inserted_at,
+      created_at: order.inserted_at,
+      updated_at: order.updated_at,
       status: order.status,
       seller_id: order.seller_id,
       buyer_user_id: order.buyer_user_id,
@@ -74,31 +75,14 @@ defmodule FoodFromHomeWeb.OrderJSON do
   defp data(seller = %Seller{}) do
     %{
       id: seller.id,
-      illustration: seller.illustration,
-      introduction: seller.introduction
+      nickname: seller.nickname
     }
   end
 
   defp data(user = %User{}) do
     %{
       id: user.id,
-      address: data(user.address),
-      phone_number: user.phone_number,
-      email_id: user.email_id,
-      first_name: user.first_name,
-      gender: user.gender,
-      last_name: user.last_name,
-      profile_image: user.profile_image
-    }
-  end
-
-  defp data(address = %Address{}) do
-    %{
-      door_number: address.door_number,
-      street: address.street,
-      city: address.city,
-      country: address.country,
-      postal_code: address.postal_code
+      email_id: user.email_id
     }
   end
 
@@ -118,7 +102,14 @@ defmodule FoodFromHomeWeb.OrderJSON do
       id: food_menu.id,
       name: food_menu.name,
       price: food_menu.price,
-      rebate: food_menu.rebate
+      rebate: data(food_menu.rebate)
+    }
+  end
+
+  defp data(rebate = %Rebate{}) do
+    %{
+      count: rebate.count,
+      discount_percentage: rebate.discount_percentage
     }
   end
 
@@ -136,7 +127,6 @@ defmodule FoodFromHomeWeb.OrderJSON do
     %{
       id: delivery.id,
       picked_up_at: delivery.picked_up_at,
-      current_geoposition: delivery.current_geoposition,
       delivered_at: delivery.delivered_at
     }
   end
