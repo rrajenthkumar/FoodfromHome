0944a5cd1536887a08b2dabeabdbab901bc9afb0
rajenth <rkumar@aidminutes.com>
2024-01-09 10:28:53 +0100

    kafka wip

--- backend/food_from_home/lib/food_from_home/kafka_agent/producer.ex
+++ backend/food_from_home/lib/food_from_home/kafka_agent/producer.ex
@@ -6,24 +6,24 @@ defmodule FoodFromHome.KafkaAgent.Producer do
 
   Topics "sellers_searched", "seller_viewed", "food_menu_viewed" are used for seller metrics.
   """
-  # @permitted_topics [
-  #   "sellers_searched",
-  #   "seller_viewed",
-  #   "food_menu_viewed",
-  #   "order_confirmed",
-  #   "order_cancelled",
-  #   "payment_failed",
-  #   "payment_cancelled",
-  #   "delivery_started",
-  #   "delivery_completed",
-  #   "review_added"
-  # ]
+  @permitted_topics [
+    "sellers_searched",
+    "seller_viewed",
+    "food_menu_viewed",
+    "order_confirmed",
+    "order_cancelled",
+    "payment_failed",
+    "payment_cancelled",
+    "delivery_started",
+    "delivery_completed",
+    "review_added"
+  ]
 
-  # def call(topic, key, message) when topic in @permitted_topics and is_binary(key) do
-  #   KafkaEx.produce(topic, key, message)
-  # end
+  def call(topic, key, message) when topic in @permitted_topics and is_binary(key) do
+    KafkaEx.produce(topic, key, message)
+  end
 
-  # def call(topic, _key, _message) when topic not in @permitted_topics do
-  #   {:error, "The topic '#{topic}' is not permitted"}
-  # end
+  def call(topic, _key, _message) when topic not in @permitted_topics do
+    {:error, "The topic '#{topic}' is not permitted"}
+  end
 end
